d8d82a55691e86c87e254135f565e6dc6529e10c
sjthn <srijith14m@gmail.com>
2023-11-08 10:43:02 +0530

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Source_Code_Pro } from "next/font/google";
+import Script from "next/script";
 import App from "./_app";
 import styles from "./styles.module.css";
 
@@ -15,6 +16,16 @@ export default function RootLayout({
     <html lang="en" className={sourceCodePro.className}>
       <body className={styles.body}>
         <App children={children} />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=UA-160355954-1"></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-160355954-1');
+          `}
+        </Script>
       </body>
     </html>
   );
