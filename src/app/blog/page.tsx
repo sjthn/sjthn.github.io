ca56711ec56ec213741e450bae8f764567f0ec6b
sjthn <srijith14m@gmail.com>
2023-11-09 18:12:23 +0530

    Reformat the blog post publish date

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -15,7 +15,7 @@ export default function Blog() {
             <Link href="/blog/2023-10-19-improve-your-coding-productivity-through-keyboard-shortcuts">
               Efficient coding using keyboard shortcuts - Android Studio
             </Link>
-            <div>Published on: Nov 2, 2023</div>
+            <div className={styles.post_list_date}>- Nov 2, 2023</div>
           </li>
         </div>
 
@@ -24,7 +24,7 @@ export default function Blog() {
             <Link href="/blog/2019-11-17-jekyll-setup">
               Steps to setup a blog using Jekyll
             </Link>
-            <div>Published on: Nov 17, 2019</div>
+            <div className={styles.post_list_date}>- Nov 17, 2019</div>
           </li>
         </div>
 
@@ -33,7 +33,7 @@ export default function Blog() {
             <Link href="/blog/2019-03-19-a-case-with-kotlin-let">
               A case with Kotlin let
             </Link>
-            <div>Published on: Mar 19, 2019</div>
+            <div className={styles.post_list_date}>- Mar 19, 2019</div>
           </li>
         </div>
 
@@ -42,7 +42,7 @@ export default function Blog() {
             <Link href="/blog/2018-02-13-creating-a-news-feed-list-using-facebook-lithos-sections-api">
               Creating a News Feed list using Facebook Litho’s Sections API
             </Link>
-            <div>Published on: Feb 13, 2018</div>
+            <div className={styles.post_list_date}>- Feb 13, 2018</div>
           </li>
         </div>
 
@@ -51,7 +51,7 @@ export default function Blog() {
             <Link href="/blog/2017-12-18-how-to-learn-react-native-for-android-ios-devs">
               How to learn React Native – For Android/iOS devs
             </Link>
-            <div>Published on: Dec 18, 2017</div>
+            <div className={styles.post_list_date}>- Dec 18, 2017</div>
           </li>
         </div>
 
@@ -60,7 +60,7 @@ export default function Blog() {
             <Link href="/blog/2017-11-27-basics-of-facebook-litho-a-declarative-ui-framework-for-android">
               Basics of Facebook Litho – a Declarative UI framework for Android
             </Link>
-            <div>Published on: Nov 27, 2017</div>
+            <div className={styles.post_list_date}>- Nov 27, 2017</div>
           </li>
         </div>
 
@@ -69,7 +69,7 @@ export default function Blog() {
             <Link href="/blog/2017-11-09-handle-data-change-in-recyclerview-using-diffutil">
               Handling data change in RecyclerView gracefully using DiffUtil
             </Link>
-            <div>Published on: Nov 9, 2017</div>
+            <div className={styles.post_list_date}>- Nov 9, 2017</div>
           </li>
         </div>
 
@@ -78,7 +78,7 @@ export default function Blog() {
             <Link href="/blog/2017-10-24-android-recyclerview-drag-and-drop-and-swipe-to-dismiss">
               Android RecyclerView – Drag and Drop and Swipe to Dismiss
             </Link>
-            <div>Published on: Oct 24, 2017</div>
+            <div className={styles.post_list_date}>- Oct 24, 2017</div>
           </li>
         </div>
 
@@ -87,7 +87,7 @@ export default function Blog() {
             <Link href="/blog/2017-10-17-android-recyclerview-expandable-headers">
               Android RecyclerView – Expandable Headers
             </Link>
-            <div>Published on: Oct 17, 2017</div>
+            <div className={styles.post_list_date}>- Oct 17, 2017</div>
           </li>
         </div>
 
@@ -96,7 +96,7 @@ export default function Blog() {
             <Link href="/blog/2017-10-12-android-recyclerview-itemdecoration-and-itemanimator">
               Android RecyclerView – ItemDecoration and ItemAnimator
             </Link>
-            <div>Published on: Oct 12, 2017</div>
+            <div className={styles.post_list_date}>- Oct 12, 2017</div>
           </li>
         </div>
 
@@ -105,7 +105,7 @@ export default function Blog() {
             <Link href="/blog/2017-10-09-android-recyclerview-the-basics">
               Android RecyclerView - The Basics
             </Link>
-            <div>Published on: Oct 9, 2017</div>
+            <div className={styles.post_list_date}>- Oct 9, 2017</div>
           </li>
         </div>
       </ul>
